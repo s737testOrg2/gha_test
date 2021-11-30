f65e862ec85de3cc33492da4cc3c23d46927c519
sergioko737 <s29585476@gmail.com>
2021-11-30 17:37:07 +0200

    dummy

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -17,4 +17,4 @@ concat!("[#{:?}] 🗳  ", $patterasdassssdsan), <frame_system::Pallet<T>>::block
 fdasdfdasertertrewadsadsdasdsafasdffdsgdsgds
 fdasdfdasertertrewadsadsdasdsafasdffdsgdsgdsdfads
 dsdsdsdfdsfdsa
-dfsadsadsada
+dfsadsadsadadfsdfdsf
