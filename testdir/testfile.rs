32b1110e0d9f6be98dfdd07cc03cd442605aac07
sergioko737 <s29585476@gmail.com>
2021-12-09 13:54:05 +0200

    Try team requests

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -17,4 +17,4 @@ concat!("[#{:?}] 🗳  ", $patterasdassssdsan), <frame_system::Pallet<T>>::block
 fdasdfdasertertrewadsadsdasdsafasdffdsgdsfdasdf
 dsdfsfdssdfdsadfsdaffadsfadssadsdsdds
 lkljkjlkj
-dsfdfdsfdfdfdsafdsf
+dsfdfdsfdfdfdsafdsfdsafdf
