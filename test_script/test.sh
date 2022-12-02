303d2783ed59b07171f2d8a7115d28d40d9996e3
Sergej Kostjucenko <s29585476@gmail.com>
2022-12-02 14:01:46 +0200

    Update test.sh

--- test_script/test.sh
+++ test_script/test.sh
@@ -4,4 +4,4 @@ echo "------------------------------------------------------------------------"
 echo "ENV VAR IN ECHO $GITHUB_REPOSITORY_OWNER"
 echo "--------------------------- next we'll test curl"
 echo "https://raw.githubusercontent.com/$GITHUB_REPOSITORY_OWNER/gha_test/master/testdir/testfile3"
-curl "https://raw.githubusercontent.com/$GITHUB_REPOSITORY_OWNER/gha_test/master/testdir/testfile3"
+curl https://raw.githubusercontent.com/$GITHUB_REPOSITORY_OWNER/gha_test/master/testdir/testfile3
